11c7001ae89db86a633ef5a3d45890830c519b0a
MarcosC1266 <marcos04cabrera@gmail.com>
2025-11-03 21:23:13 -0400

    frontend: esquema de tabala, agregar produto, actualizar, eliminar y llamadas a la api

--- master-workshop-frontend/src/app/app.ts
+++ master-workshop-frontend/src/app/app.ts
@@ -1,12 +1,138 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgClass } from "@angular/common";
+import { ProductManager } from './services/product-manager';
 
+
+export interface Producto {
+  id: number
+  nombre: string
+  descripcion: string
+  precio: number
+  cantidad: number
+  categoria: string
+}
+
+export interface ProductoForm {
+  nombre: string
+  descripcion: string
+  precio: string
+  cantidad: string
+  categoria: string
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'master-workshop-frontend';
+export class App implements OnInit {
+
+  private productManager = inject(ProductManager)
+
+  products = signal<Producto[]>([])
+  categorias = ['Computadoras', 'Accesorios', 'Monitores', 'Audio', 'Almacenamiento', 'Redes', 'Otros'];
+  showAddForm = signal(false)
+  showEditDialog = signal(false)
+  showDeleteWarning = signal(false)
+  selectedProduct = signal<Producto | null>(null)
+
+  formData = signal<ProductoForm>({
+    nombre: '',
+    descripcion: '',
+    precio: '',
+    cantidad: '',
+    categoria: ''
+  })
+
+  ngOnInit(): void {
+    this.getAllProducts()
+  }
+
+  getAllProducts() {
+    this.productManager.getAllProducts().subscribe({
+      next: values => {
+        this.products.set(values)
+      }
+    })
+  }
+
+  handleInputChange(e: HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement) {
+    if (e) {
+      this.formData.set({ ...this.formData(), [e.name]: e.value });
+    }
+  };
+
+  handleAddProduct(e: Event) {
+    e.preventDefault();
+    const newProduct = {
+      id: 0,
+      ...this.formData(),
+      precio: parseFloat(this.formData().precio),
+      cantidad: parseInt(this.formData().cantidad)
+    };
+    this.productManager.addNewProduct(newProduct).subscribe({
+      next: result => {
+        if (!result) return
+        this.formData.set({ nombre: '', descripcion: '', precio: '', cantidad: '', categoria: '' });
+        this.showAddForm.set(false);
+        this.getAllProducts()
+      }
+    })
+  };
+
+  handleEditClick(product: Producto) {
+    this.selectedProduct.set(product);
+    this.formData.set({
+      nombre: product.nombre,
+      descripcion: product.descripcion,
+      precio: product.precio.toString(),
+      cantidad: product.cantidad.toString(),
+      categoria: product.categoria
+    });
+    this.showEditDialog.set(true);
+  };
+
+  handleEditProduct(e: Event) {
+    e.preventDefault();
+    this.products.set(this.products().map(p =>
+      this.selectedProduct() != null && p.id === this.selectedProduct()?.id
+        ? { ...p, ...this.formData(), precio: parseFloat(this.formData().precio), cantidad: parseInt(this.formData().cantidad) }
+        : p
+    ));
+
+    const updatedProduct: Producto = {
+      id: this.selectedProduct()?.id ?? 0,
+      ...this.formData(),
+      precio: parseFloat(this.formData().precio),
+      cantidad: parseInt(this.formData().cantidad)
+    };
+    this.productManager.updateProduct(updatedProduct).subscribe({
+      next: response => {
+        if (!response) return
+        this.showEditDialog.set(false);
+        this.selectedProduct.set(null);
+        this.formData.set({ nombre: '', descripcion: '', precio: '', cantidad: '', categoria: '' });
+        this.getAllProducts()
+      }
+    })
+
+  };
+
+  handleDeleteClick(product: Producto) {
+    this.selectedProduct.set(product);
+    this.showDeleteWarning.set(true);
+  };
+
+  confirmDelete() {
+    this.productManager.deleteProduct(this.selectedProduct()?.id ?? 0).subscribe({
+      next: res => {
+        if (!res) return
+        this.showDeleteWarning.set(false);
+        this.selectedProduct.set(null);
+        this.getAllProducts()
+      }
+    })
+
+  };
 }
---
+++ master-workshop-frontend/src/app/services/product-manager.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Producto } from '../app';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductManager {
+  private http = inject(HttpClient)
+  private apiUrl = "http://localhost:5002/api/Products"
+
+  getAllProducts(): Observable<Producto[]> {
+    return this.http.get<Producto[]>(this.apiUrl)
+  }
+
+  getProductById(id: number): Observable<Producto> {
+    return this.http.get<Producto>(`${this.apiUrl}/${id}`)
+  }
+
+  addNewProduct(producto: Producto): Observable<boolean> {
+    return this.http.post<boolean>(this.apiUrl, producto)
+  }
+
+  updateProduct(producto: Producto): Observable<boolean> {
+    return this.http.put<boolean>(`${this.apiUrl}/update`, producto)
+  }
+
+  deleteProduct(id: number): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.apiUrl}/delete/${id}`)
+  }
+}
